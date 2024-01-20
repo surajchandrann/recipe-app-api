5c0baa087df28dd142f85f056d409958f26df4fe
surajchandrann <surajchandran23.sc@gmail.com>
2024-01-20 17:48:16 +0530

    Added psycopg2

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculatr functions
+"""
+
+def add(x,y):
+    return x+y
+
+
+def subtract(x,y):
+    return y-x
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample tests
+
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    
+    def test_add_numbers(self):
+        
+        res = calc.add(5,6)
+        self.assertEqual(res,11)
+        
+    def test_subtract_numbers(self):
+        res = calc.subtract(1,2)
+        self.assertEqual(res,1)
