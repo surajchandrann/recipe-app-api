a0725f8d1af733e743a83559231d7e71b39edada
surajchandrann <surajchandran23.sc@gmail.com>
2024-02-01 12:26:48 +0530

    Configured Docker compose, checks to wait for db and created default tables by running migrations

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,10 @@
 Calculatr functions
 """
 
-def add(x,y):
+
+def add(x, y):
     return x+y
 
 
-def subtract(x,y):
+def subtract(x, y):
     return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,13 +6,12 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    
     def test_add_numbers(self):
-        
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
-        
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
     def test_subtract_numbers(self):
-        res = calc.subtract(1,2)
-        self.assertEqual(res,1)
+        res = calc.subtract(1, 2)
+        self.assertEqual(res, 1)
